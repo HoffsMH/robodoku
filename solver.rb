497a4d4c2cea4ffc36d8184df2035429d15d3d7a
HoffsMH <hecker.matthew@gmail.com>
2015-07-14 21:24:44 -0600

    have the basic 2d array working time to move on to other functions.

--- solver.rb
+++ solver.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Solver
 
 end
@@ -6,19 +7,21 @@ end
 class Puzzle
   attr_accessor :cells
   def initialize
-    @cells = [[]]
+    @cells = []
     
   end
   def load(puzzle_string_array)
 
-    puzzle_string_array.each_with_index do |line, line_number|
-      line.split('').each do |char|
-        @cells[line] <<
+    @cells = puzzle_string_array.map.with_index(0) do |line, line_number|     
+
+        row_of_cells = line.split('').map do |char|
+          Cell.new(char)
+        end
+        row_of_cells
         
-      end
-      
-      
+        
     end
+
   end
 
   
@@ -33,11 +36,12 @@ class Puzzle
 end
 class Cell
   attr_accessor :value, :possibilities, :is_a_given
-  def initialize(value, coord)
+  def initialize(value)
     @value =  value
     @possibilities = [1..9]
     @is_a_given = true
   end
+  
 end
 
 #ask dont tell
@@ -46,3 +50,5 @@ end
 # puzzle.cell[0,8].possibilities => [0,4,1,5]
 # puzzle.cell[0,8].get_row_neighbors => [0,9,3]
 # puzzle.cell[0,8].c
+      # 0                         1
+# [[cell0, cell1, cell2], [cell0,cell1,cell2]]
--- solver_test.rb
+++ solver_test.rb
@@ -7,13 +7,13 @@ require 'minitest/spec'
 require_relative 'solver'
 
 describe Solver do
-
-
+  
+  
   describe ".solve(puzzle string)" do
     it "hands a puzzle object to load()"
     it "returns a puzzle object"
   end
-
+  
   describe '.deduce(cell)' do
     it "returns list of numbers that could exist in a given cell coordinate"
   end
@@ -25,22 +25,39 @@ describe Solver do
     it "writes to a cell's possibilities"
     
   end
-
+  
 end
 
+
 describe Cell do
+  describe '.new(value)' do
+    it "instantiates a new cell object" do
+      cell0 = Cell.new('h')
+      expected = Cell
+      result = cell0.class
+      
+      assert_equal(expected, result)
+      
+    end
+  end
   describe '.value' do
-    it 'it returns the value of the given cell'
+    it 'it returns the value of the given cell' do
+      cell0 = Cell.new('h')
+      expected = 'h'
+      result = cell0.value
+      
+      assert_equal(expected, result)
+    end
     
   end
   
   describe ".get_column_neighbors(cell)" do
     it "returns the value of every non blank cell in a column for a given cell as an array"
-
+    
   end
   describe ".get_row_neighbors(cell)" do
     it "returns the value of every non blank cell in a row for a given cell as an array"
-
+    
   end
   describe ".get_box_neighbors(cell)" do
     it "returns the value of every non blank cell in a zone for a given cell as an array"
@@ -57,26 +74,53 @@ describe Cell do
   
 end
 describe Puzzle do
-
+  
   describe ".add_trailing_whitespace(line)" do
     it "adds trailing whitespace to line before parsing it into cell objects" do
+      puzzle = Puzzle.new
+      line_string = " 1 9
+"
       
+      expected = " 1 9     "
+      result = puzzle.add_trailing_whitespace(line_string)
       
+      assert_equal(expected, result)
     end  
     
   end
   describe ".load(puzzleString)" do
-    puzzle = Puzzle.new
-    puzzle_string_array = File.readlines('solved_puzzles/puzzle1.txt')
-    puzzle.load(puzzle_string_array)
-    
-    result = puzzle.cells[0][3].value
-    
+    it "loads a single row of puzzle objects" do
+      
+      puzzle = Puzzle.new
+      puzzle_string_row = " 8  2 3"
+      puzzle_string_array = []
+      puzzle_string_array << puzzle_string_row
+      puzzle.load(puzzle_string_array)
+      
+      result = puzzle.cells[0][1].value
+      expected = '8'
+      
+      assert_equal(expected, result)
+      
+      
+    end
+    it "loads a full sudoku puzzle of cell objects" do
+      skip
+      puzzle = Puzzle.new
+      puzzle_string_array = File.readlines('solved_puzzles/puzzle1.txt')
+      # puzzle_string_
+      puzzle.load(puzzle_string_array)
+      
+      expected = '4'
+      result = puzzle.cells[4][1].value
+      
+      assert_equal(expected, result)
+    end    
     
     
   end
   
   
-
-
+  
+  
 end
